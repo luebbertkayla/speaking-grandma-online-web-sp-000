a290a8dae1c621eca5f4a876187a8ed54eb64efd
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 01:24:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ phrase = ""
 
   def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+    
   elsif
     phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
