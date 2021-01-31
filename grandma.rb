3679020c4169ab670279aad7fb459ee493421cf7
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 01:25:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,12 +16,11 @@ phrase = ""
 
   def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif
     phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+   return "HUH?! SPEAK UP, SONNY!"
   end
 end
